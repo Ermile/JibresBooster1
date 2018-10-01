ace51636b254ecc6f88fffe37aff2333b694dac3
Javad Kakroudi <J.Evazzadeh@gmail.com>
2018-10-02 01:03:31 +0330

    run localhost for test

--- JibresBooster1/MainWindow.xaml.cs
+++ JibresBooster1/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net;
+using System.Threading;
 
 namespace JibresBooster1
 {
@@ -23,6 +25,7 @@ namespace JibresBooster1
         public MainWindow()
         {
             InitializeComponent();
+            runListener();
         }
 
         private void button_Click(object sender, RoutedEventArgs e)
@@ -30,5 +33,43 @@ namespace JibresBooster1
             Console.Beep();
             System.Diagnostics.Process.Start("https://jibres.com");
         }
+
+
+        static HttpListener _httpListener = new HttpListener();
+        static void runListener()
+        {
+            MessageBox.Show("Starting server...");
+            // add prefix "http://localhost:5000/"
+            _httpListener.Prefixes.Add("http://localhost:4200/");
+            // start server (Run application as Administrator!)
+            _httpListener.Start();
+            MessageBox.Show("Server started.");
+            Thread _responseThread = new Thread(ResponseThread);
+            // start the response thread
+            _responseThread.Start(); 
+        }
+
+        static void ResponseThread()
+        {
+            while (true)
+            {
+                HttpListenerContext context = _httpListener.GetContext(); // get a context
+                // Now, you'll find the request URL in context.Request.Url
+                // get the bytes to response
+                byte[] _responseArray = Encoding.UTF8.GetBytes("<html><head><title>Localhost server -- port 5000</title></head>" +
+                    "<body>Welcome to the <strong>Localhost server</strong> -- <em>port 5000!</em></body></html>");
+                // write bytes to the output stream
+                context.Response.OutputStream.Write(_responseArray, 0, _responseArray.Length);
+                // set the KeepAlive bool to false
+                context.Response.KeepAlive = false;
+                // close the connection
+                context.Response.Close();
+
+                MessageBox.Show("Respone given to a request.");
+                // Console.WriteLine("Respone given to a request.");
+            }
+        }
+
+
     }
 }
