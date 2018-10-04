cf3d972ad8caf57e4239e3696b656a9f6901c6b9
Javad Kakroudi <J.Evazzadeh@gmail.com>
2018-10-05 01:03:39 +0330

    add new class to use kiccc

---
+++ JibresBooster1/PcPos/JibresKiccc.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+using System.Text.RegularExpressions;
+using Kiccc.Ing.PcPos;
+using Kiccc.Ing.PcPos.Serial;
+
+namespace JibresBooster1.PcPos
+{
+    class Kiccc
+    {
+        public static void init()
+        {
+            Console.Beep();
+
+
+        }
+
+    }
+}
