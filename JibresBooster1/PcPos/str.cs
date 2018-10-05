87513fc867b1fb8b30b88b4a3da94723ab68c8c0
Javad Kakroudi <J.Evazzadeh@gmail.com>
2018-10-05 04:08:38 +0330

    bugfix str

--- JibresBooster1/PcPos/str.cs
+++ JibresBooster1/PcPos/str.cs
@@ -7,11 +7,16 @@ namespace JibresBooster1.PcPos
     {
         public static string Left(this string value, int maxLength)
         {
+            if (string.IsNullOrEmpty(value))
+            {
+                return value;
+            }
             value = value.Trim();
             if (string.IsNullOrEmpty(value))
             {
                 return value;
             }
+
             maxLength = Math.Abs(maxLength);
 
             return (value.Length <= maxLength ? value : value.Substring(0, maxLength));
