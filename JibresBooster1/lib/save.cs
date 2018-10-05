67066a23a5298a14c1dad44eed515bbb251bd4de
Javad Kakroudi <J.Evazzadeh@gmail.com>
2018-10-06 01:46:56 +0330

    add save lib

---
+++ JibresBooster1/lib/save.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JibresBooster1.lib
+{
+    class save
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        public static async Task<bool> post(Dictionary<string, string> _vals)
+        {            
+            try
+            {
+                var serverURL = "https://jibres.com/hook/booster1/";
+                var content = new FormUrlEncodedContent(_vals);
+
+                var response = await client.PostAsync(serverURL, content);
+
+                var responseString = await response.Content.ReadAsStringAsync();
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }            
+        }
+
+
+
+        public static async Task<bool> get(Dictionary<string, string> _vals)
+        {
+            try
+            {
+                var serverURL = "https://jibres.com/hook/booster1/";
+
+                var responseString = await client.GetStringAsync(serverURL);
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
