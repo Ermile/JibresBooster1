036b97e99d89d22f8b3599d94a66d6133b8d7423
Javad Kakroudi <J.Evazzadeh@gmail.com>
2018-10-13 02:59:42 +0330

    complete sample

--- JibresBooster1/Forms/Generator/Kiccc.xaml.cs
+++ JibresBooster1/Forms/Generator/Kiccc.xaml.cs
@@ -19,10 +19,13 @@ namespace JibresBooster1.Forms.Generator
     /// </summary>
     public partial class Kiccc : Window
     {
+        private string testLink;
         public Kiccc()
         {
             InitializeComponent();
+            genereateLink();
         }
+
 
         private void Window_Initialized(object sender, EventArgs e)
         {
@@ -35,17 +38,32 @@ namespace JibresBooster1.Forms.Generator
             }
         }
 
+
         private void Window_KeyUp(object sender, KeyEventArgs e)
         {
+            genereateLink();
+        }
+
+
+        private void genereateLink()
+        {
             // show sale box
+            string linkStr = lib.listener.JibresLocalServer;
+            linkStr += "?type=PcPosKiccc";
+            linkStr += "&serial=" + txtSerial.Text;
+            linkStr += "&terminal=" + txtTerminal.Text;
+            linkStr += "&acceptor=" + txtAcceptor.Text;
+            linkStr += "&port=" + txtPort.Text;
+            linkStr += "&sum=" + txtSum.Text;
 
-            string str = "";
-            str += "?serial=" + txtSerial.Text;
-            str += "&terminal=" + txtTerminal.Text;
-            str += "&acceptor=" + txtAcceptor.Text;
-            str += "&port=" + txtPort.Text;
+            txtLink.Text = linkStr;
+            testLink = linkStr;
+        }
 
 
+        private void btnSend_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(testLink);
         }
     }
 }
