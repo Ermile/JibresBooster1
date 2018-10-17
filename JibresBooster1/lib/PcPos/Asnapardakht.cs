24649433688c4c9ec6364090b354fd2673a61fb5
Javad Kakroudi <J.Evazzadeh@gmail.com>
2018-10-18 01:03:47 +0330

    add asnapardakht dll

---
+++ JibresBooster1/lib/PcPos/Asnapardakht.cs
@@ -0,0 +1,53 @@
+﻿using PosInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JibresBooster1.lib.PcPos
+{
+    //class Asnapardakht
+    public partial class Asnapardakht : ITransactionDoneHandler
+    {
+        private PCPos pcPos = new PCPos();
+
+        public static void fire()
+        {
+            
+
+        }
+
+        public void OnFinish(string _message)
+        {
+            log.save("Asanpardatkh finished. " + _message);
+            throw new NotImplementedException();
+        }
+
+        public void OnTransactionDone(TransactionResult _result)
+        {
+            log.save("Asanpardatkh Done. " + _result.ToString());
+            throw new NotImplementedException();
+        }
+
+        public void saleAsync(string _sum, string _invoice)
+        {
+            pcPos.DoASyncPayment(_sum, string.Empty, _invoice, DateTime.Now, this);
+        }
+
+
+        private void initLan(string _ip, int _port = 17000)
+        {
+            pcPos.InitLAN(_ip, _port);
+            log.save("init asanpardatkh on lan with ip address " + _ip);
+        }
+
+
+        private void initSerial(string _com)
+        {
+            pcPos.InitSerial(_com, 115200);
+            log.save("init asanpardatkh on com " + _com);
+        }
+
+    }
+}
