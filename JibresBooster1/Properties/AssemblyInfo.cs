7e754d87be8dc58f34cb5990e3b583b91e371e5a
Javad Kakroudi <J.Evazzadeh@gmail.com>
2018-11-15 05:12:09 +0330

    change some assembly values

--- JibresBooster1/Properties/AssemblyInfo.cs
+++ JibresBooster1/Properties/AssemblyInfo.cs
@@ -7,13 +7,13 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("JibresBooster1")]
+[assembly: AssemblyTitle("جیبرس بوستر")]
 [assembly: AssemblyDescription("Boost power of Jibres to break windows!")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Ermile")]
-[assembly: AssemblyProduct("JibresBooster1")]
+[assembly: AssemblyCompany("Ermile | ارمایل")]
+[assembly: AssemblyProduct("Jibres Booster")]
 [assembly: AssemblyCopyright("Copyright Ermile©  2018")]
-[assembly: AssemblyTrademark("Jibres")]
+[assembly: AssemblyTrademark("Jibres | جیبرس")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
