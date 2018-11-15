2803c16f7d79d7b0cb5b0ad4a1040ca29f955240
Javad Kakroudi <J.Evazzadeh@gmail.com>
2018-11-15 05:09:53 +0330

    add aboutbox

---
+++ JibresBooster1/Forms/AboutBox1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Reflection;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace JibresBooster1.Forms
+{
+    partial class AboutBox1 : Form
+    {
+        public AboutBox1()
+        {
+            InitializeComponent();
+
+            Assembly executingAssembly = Assembly.GetExecutingAssembly();
+            var fieVersionInfo = FileVersionInfo.GetVersionInfo(executingAssembly.Location);
+            var version = fieVersionInfo.FileVersion;
+
+
+            Text = String.Format("About {0}", fieVersionInfo.ProductName);
+            labelProductName.Text = fieVersionInfo.ProductName;
+            labelVersion.Text = String.Format("Version {0}", fieVersionInfo.ProductVersion);
+            labelCopyright.Text = fieVersionInfo.LegalCopyright;
+            labelCompanyName.Text = fieVersionInfo.CompanyName;
+            // this.textBoxDescription.Text = fieVersionInfo.FileDescription;
+        }
+    }
+}
